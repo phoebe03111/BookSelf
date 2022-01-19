7720e1781dc92010d994af7a86326369a6fe0395
Phoebe Chang <phoebe03111@gmail.com>
2022-01-19 13:41:29 -0800

    Setup routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Header from "../src/components/Header/Header";
+import BookDetailPage from "../src/pages/BookDetailPage/BookDetailPage";
+import AddBookPage from "../src/pages/AddBookPage/AddBookPage";
+import BooksPage from "../src/pages/BooksPage/BooksPage";
+import HomePage from "../src/pages/HomePage/HomePage";
+
 function App() {
-  return <h1>App</h1>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/books/:booksId" component={BookDetailPage} />
+        <Route path="/books/add" exact component={AddBookPage} />
+        <Route path="/books" exact component={BooksPage} />
+        <Route path="/" exact component={HomePage} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/pages/AddBookPage/AddBookPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AddBookPage() {
+  return <div>Add Book Page</div>;
+}
+
+export default AddBookPage;
