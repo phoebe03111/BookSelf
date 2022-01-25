49e73016ef62bee1e42f9bbd953e219299d049ef
Phoebe Chang <phoebe03111@gmail.com>
2022-01-24 22:46:00 -0800

    Install material UI for textfield and button components on login page

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,33 @@ import Header from "../src/components/Header/Header";
 import BookDetailPage from "../src/pages/BookDetailPage/BookDetailPage";
 import BooksPage from "../src/pages/BooksPage/BooksPage";
 import HomePage from "../src/pages/HomePage/HomePage";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { purple } from "@mui/material/colors";
 import "./styles/App.scss";
 import BooksDemoPage from "./pages/BooksDemoPage/BooksDemoPage";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#475847",
+    },
+    secondary: purple,
+  },
+});
+
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route path="/books/:booksId" component={BookDetailPage} />
-        <Route path="/books-demo" exact component={BooksDemoPage} />
-        <Route path="/books" exact component={BooksPage} />
-        <Route path="/" exact component={HomePage} />
-      </Switch>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/books/:booksId" component={BookDetailPage} />
+          <Route path="/books-demo" exact component={BooksDemoPage} />
+          <Route path="/books" exact component={BooksPage} />
+          <Route path="/" exact component={HomePage} />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
+import { TextField, Button } from "@mui/material";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import "./LoginForm.scss";
 
 const baseUrl = "http://localhost:8080";
@@ -10,17 +12,34 @@ const signupUrl = `${baseUrl}/signup`;
 function LoginForm() {
   const [isSignedUp, setIsSignedUp] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [isLogInError, setIsLogInError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
+  const [loginUsername, setLoginUsername] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [loginUsernameError, setLoginUsernameError] = useState(false);
+  const [loginPasswordError, setLoginPasswordError] = useState(false);
+  const [signupUsername, setSignupUsername] = useState("");
+  const [signupEmail, setSignupEmail] = useState("");
+  const [signupPassword, setSignupPassword] = useState("");
 
   let history = useHistory();
 
   const handleLogin = (e) => {
     e.preventDefault();
+
+    setLoginUsernameError(false);
+    setLoginPasswordError(false);
+
+    if (!loginUsername) {
+      setLoginUsernameError(true);
+    }
+
+    if (!loginPassword) {
+      setLoginPasswordError(true);
+    }
+
     axios
       .post(loginUrl, {
-        username: e.target.username.value,
-        password: e.target.password.value,
+        username: loginUsername,
+        password: loginPassword,
       })
       .then((res) => {
         sessionStorage.setItem("token", res.data.token);
@@ -28,18 +47,18 @@ function LoginForm() {
         // setIsLoggedIn(true);
       })
       .catch((err) => {
-        setIsLogInError(true);
-        setErrorMessage(err);
+        console.log(err);
       });
   };
 
   const handleSignup = (e) => {
     e.preventDefault();
+
     axios
       .post(signupUrl, {
-        username: e.target.username.value,
-        email: e.target.email.value,
-        password: e.target.password.value,
+        username: signupUsername,
+        email: signupEmail,
+        password: signupPassword,
       })
       .then((res) => {
         setIsSignedUp(true);
@@ -53,29 +72,62 @@ function LoginForm() {
         <h1 className="auth__title">Sign up</h1>
         <form className="form" onSubmit={handleSignup}>
           <div className="form-group">
-            Username:{" "}
-            <input type="text" name="username" className="form__input" />
+            <TextField
+              id="outlined-basic"
+              label="Username"
+              variant="outlined"
+              fullWidth
+              required
+              value={signupUsername}
+              onChange={(e) => setSignupUsername(e.target.value)}
+            />
           </div>
 
           <div className="form-group">
-            Email: <input type="email" name="email" className="form__input" />
+            <TextField
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+              fullWidth
+              required
+              value={signupEmail}
+              onChange={(e) => setSignupEmail(e.target.value)}
+            />
           </div>
 
           <div className="form-group">
-            Password:{" "}
-            <input type="password" name="password" className="form__input" />
+            <TextField
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+              fullWidth
+              required
+              value={signupPassword}
+              onChange={(e) => setSignupPassword(e.target.value)}
+            />
           </div>
 
-          <button className="btn" type="submit">
-            Sign up
-          </button>
+          <Button
+            type="submit"
+            color="primary"
+            variant="contained"
+            endIcon={<KeyboardArrowRightIcon />}
+            onClick={() => setIsSignedUp(false)}
+          >
+            Signup
+          </Button>
         </form>
 
         <p className="redirect">
           Already have an account?{" "}
-          <button className="btn" onClick={() => renderLogin()}>
-            Log in
-          </button>
+          <Button
+            color="primary"
+            variant="contained"
+            endIcon={<KeyboardArrowRightIcon />}
+            onClick={renderLogin}
+          >
+            Login
+          </Button>
         </p>
       </div>
     );
@@ -85,25 +137,48 @@ function LoginForm() {
     return (
       <div className="auth">
         <h1 className="auth__title">Login</h1>
-        {isLogInError && <label style={{ color: "red" }}>{errorMessage}</label>}
         <form className="form" onSubmit={handleLogin}>
           <div className="form-group">
-            Username:{" "}
-            <input type="text" name="username" className="form__input" />
+            <TextField
+              id="outlined-basic"
+              label="Username"
+              variant="outlined"
+              fullWidth
+              value={loginUsername}
+              onChange={(e) => setLoginUsername(e.target.value)}
+              error={loginUsernameError}
+            />
           </div>
           <div className="form-group">
-            Password:{" "}
-            <input type="password" name="password" className="form__input" />
+            <TextField
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+              fullWidth
+              value={loginPassword}
+              onChange={(e) => setLoginPassword(e.target.value)}
+              error={loginPasswordError}
+            />
           </div>
-          <button className="btn" type="submit">
+          <Button
+            type="submit"
+            color="primary"
+            variant="contained"
+            endIcon={<KeyboardArrowRightIcon />}
+          >
             Login
-          </button>
+          </Button>
         </form>
         <p className="redirect">
           Don't have an account yet?{" "}
-          <button className="btn" onClick={() => renderSignup()}>
-            Sign up
-          </button>
+          <Button
+            color="primary"
+            variant="contained"
+            endIcon={<KeyboardArrowRightIcon />}
+            onClick={() => setIsLoggedIn(false)}
+          >
+            Signup
+          </Button>
         </p>
       </div>
     );
