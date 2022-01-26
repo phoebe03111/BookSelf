d653604d19d4d2f2418a8853ac7116b2eb418399
Phoebe Chang <phoebe03111@gmail.com>
2022-01-25 22:33:50 -0800

    Change books' link to external link on search result page

--- client/src/components/Book/Book.js
+++ client/src/components/Book/Book.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import placeholder from "../../assets/images/book-placeholder.jpeg";
 import "./Book.scss";
 
 function Book({ book }) {
   return (
     <div className="book__container">
-      <Link to={`/books/${book.id}`}>
+      <a href={book.volumeInfo.infoLink} target="_blank">
         <img
           className="book__book"
           src={
@@ -15,7 +14,7 @@ function Book({ book }) {
               : placeholder
           }
         />
-      </Link>
+      </a>
     </div>
   );
 }
