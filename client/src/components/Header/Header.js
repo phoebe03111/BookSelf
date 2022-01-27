9cf19af3116ec3490d0f42ac49b08880014bf848
Phoebe Chang <phoebe03111@gmail.com>
2022-01-26 23:36:33 -0800

    Create header for different screen sizes and add a logout button

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import logoImg from "../../assets/images/logo.png";
 import menuIcon from "../../assets/images/icons/menu-bars.svg";
 import menuCloseIcon from "../../assets/images/icons/menu-close.svg";
-import "./Header.scss";
 import NavModal from "../NavModal/NavModal";
+import { Button } from "@mui/material";
+import "./Header.scss";
 
 function Header() {
   const [showMenu, setshowMenu] = useState(false);
@@ -23,8 +24,8 @@ function Header() {
       </div>
 
       {/* Navbar */}
-      <nav className="header__nav">
-        {showMenu && <NavModal onToggle={handleToggle}/>}
+      <nav className="header__nav--mobile">
+        {showMenu && <NavModal onToggle={handleToggle} />}
 
         <div>
           <img
@@ -34,6 +35,25 @@ function Header() {
             onClick={handleToggle}
           />
         </div>
+      </nav>
+
+      <nav className="header__nav">
+        <ul className="header__nav-list">
+          <Link to="/books">
+            <li className="header__nav-item">Bookshelf</li>
+          </Link>
+          <Link to="/books/tracker">
+            <li className="header__nav-item">Tracker</li>
+          </Link>
+          <Button
+            type="submit"
+            color="success"
+            variant="contained"
+            onClick={() => sessionStorage.removeItem("token")}
+          >
+            Logout
+          </Button>
+        </ul>
       </nav>
     </header>
   );
